ae8465b1c3c6e9ecce3fef7f57571750c9d801f4
Lukas Lihotzki <l.lihotzki@famedly.com>
2025-04-17 19:20:40 +0200

    refactor(opentelemetry): remove unused import

--- synapse/logging/handlers.py
+++ synapse/logging/handlers.py
@@ -92,7 +92,6 @@ class PeriodicallyFlushingMemoryHandler(MemoryHandler):
 
 
 try:
-    from opentelemetry._logs import set_logger_provider
     from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
     from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
     from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
