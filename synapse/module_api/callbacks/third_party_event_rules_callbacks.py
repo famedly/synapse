0b8abb80155b9bf31c20c49140ea4d5213ab2750
FrenchGithubUser <t.traineau@famedly.com>
2026-06-25 16:29:26 +0200

    fix: revert frozen-event workaround superseded by Rust Event port
    upstream v1.155 ported the Event class to Rust, removing freeze() and
    unfreeze() from the Python API. Our workaround used a pattern solely to
    guarantee event.unfreeze() always ran. That pattern is not necessary
    anymore. Revert to upstream's state.

--- synapse/module_api/callbacks/third_party_event_rules_callbacks.py
+++ synapse/module_api/callbacks/third_party_event_rules_callbacks.py
@@ -314,17 +314,11 @@ class ThirdPartyEventRulesModuleApiCallbacks:
             # Return if the event shouldn't be allowed or if the module came up with a
             # replacement dict for the event.
             if res is False:
-                ret = (False, None)
-                break
+                return res, None
             elif isinstance(replacement_data, dict):
-                ret = (True, replacement_data)
-                break
+                return True, replacement_data
 
-        # We can unfreeze the event unconditionally here, since incoming events should never have been frozen.
-        # This is necessary, because other parts of the code expect unfrozen events currently.
-        event.unfreeze()
-
-        return ret
+        return True, None
 
     async def on_create_room(
         self, requester: Requester, config: dict, is_requester_admin: bool
