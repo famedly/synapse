b8ab182daba1331b1ef0b02d2db1b5b3436c53b1
FrenchGithubUser <t.traineau@famedly.com>
2026-07-20 16:42:20 +0200

    fix: remove unused function
    this was missed during merge conflict resolution

--- tests/metrics/test_metrics.py
+++ tests/metrics/test_metrics.py
@@ -394,29 +394,6 @@ class LaterGaugeTests(unittest.HomeserverTestCase):
         self.assertEqual(hs2_metric_value, "2.0")
 
 
-def get_latest_metrics() -> dict[str, str]:
-    """
-    Collect the latest metrics from the registry and parse them into an easy to use map.
-    The key includes the metric name and labels.
-
-    Example output:
-    {
-        "synapse_util_caches_cache_size": "0.0",
-        "synapse_util_caches_cache_max_size{name="some_cache",server_name="hs1"}": "777.0",
-        ...
-    }
-    """
-    metric_map = {
-        x.split(b" ")[0].decode("ascii"): x.split(b" ")[1].decode("ascii")
-        for x in filter(
-            lambda x: len(x) > 0 and not x.startswith(b"#"),
-            generate_latest(REGISTRY).split(b"\n"),
-        )
-    }
-
-    return metric_map
-
-
 class ModuleInstancesInfoTests(unittest.TestCase):
     def test_module_instances_info_metric(self) -> None:
         """
